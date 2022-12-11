d987144636dc89f847a0731f2c640f2fd846ffb3
nsmchan <ehfhfh1313@naver.com>
2022-12-11 20:56:25 +0900

    수유실 처리 (gps 관련 select) (#10)
    위도랑 경도 받던거 VO 전체 받게 수정

--- ThirdProjectBackend/src/main/java/com/hd/controller/FeedingRoomController.java
+++ ThirdProjectBackend/src/main/java/com/hd/controller/FeedingRoomController.java
@@ -1,8 +1,10 @@
 package com.hd.controller;
 
+import com.google.gson.Gson;
 import com.hd.domain.FeedingRoomVO;
 import com.hd.service.FeedingRoomService;
 import lombok.extern.java.Log;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +19,36 @@ public class FeedingRoomController {
     private FeedingRoomService service;
 
     @PostMapping("/position")
-    public FeedingRoomVO getPosition(@RequestBody int fno){
+    public String getPosition(@RequestBody int fno) {
+        Gson gson = new Gson();
+        System.out.println("gson으로 바로 넘겨보자! " + fno);
+        System.out.println("gson으로 바로 넘겨보자! " + gson.toJson(service.selectPosition(fno)));
+        return gson.toJson(service.selectPosition(fno));
+    }
 
+
+    @PostMapping("/position2")
+    public FeedingRoomVO getPosition2(@RequestBody int fno){
         log.info("controller로 값이 잘 넘어왔는지?"+fno);
 //        FeedingRoomVO vo = new FeedingRoomVO();
 //        service.selectPosition(fno);
         return service.selectPosition(fno);
+    }
+
+    @PostMapping("/position3")
+    public String getPosition3(@RequestBody int fno) {
+        log.info("controller로 값이 잘 넘어왔는지?"+fno);
+        JSONObject jsonObject = new JSONObject();
+        FeedingRoomVO vo = service.selectPosition(fno);
+        jsonObject.put("fno",fno);
+        jsonObject.put("department_store",vo.getDepartment_store());
+        jsonObject.put("floor",vo.getFloor());
+        jsonObject.put("room_count",vo.getRoom_count());
+        jsonObject.put("image",vo.getImage());
+        jsonObject.put("latitude", vo.getLatitude());
+        jsonObject.put("longitude",vo.getLongitude());
+        log.info(jsonObject+"");
+        log.info(vo+"");
+        return jsonObject.toJSONString();
     }
 }
